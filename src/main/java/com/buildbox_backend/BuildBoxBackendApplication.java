9e07cefb084fc92b15f06449845ab80e52f9a680
maharsh_nayak <64581149+Maharsh-Nayak@users.noreply.github.com>
2025-12-17 16:34:12 +0530

    Project Initialization

---
+++ src/main/java/com/buildbox_backend/BuildBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buildbox_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildBoxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildBoxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buildbox_backend/BuildBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildbox_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildBoxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
